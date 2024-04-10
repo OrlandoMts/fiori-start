ec6e36e006e26b098fa3f1b91ba85d58fe5e28cc
Orlando Montes <omontes@indelpro.com>
2024-04-10 22:23:48 +0000

    2.Finish documentation

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -9,7 +9,10 @@ sap.ui.define([
 
         return Controller.extend("omontes.invoices.controller.MainView", {
             onInit: function () {
-
+                const objJson = new sap.ui.model.json.JSONModel();
+                const objView = this.getView();
+                objJson.loadData("./model/menu.json");
+                objView.setModel(objJson, "selectorScreen")
             }
         });
     });
